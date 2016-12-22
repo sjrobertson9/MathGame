e14424760b0dd2373e36f066f30bad852b68d819
Sarah Robertson <sjrobertson9@gmail.com>
2016-12-21 19:26:25 -0500

    added basic game playthrough to main method

--- UserInteraction.java
+++ UserInteraction.java
@@ -5,14 +5,55 @@ import java.util.Scanner;
  * @author lisa.yoo
  * @version 12.16.16
  */
-public class UserInteraction 
+public class UserInteraction
 {
 	private int answer;
+	private static final String INSTRUCTIONS = "Type your answer and press 'enter'.";
+	private ProblemGenerator problem;
+	private ScoreKeeper score;
 	
-	public UserInteraction()
+	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("The answer is: ");
+		int answer = in.next();
+		problem = new ProblemGenerator();
+		System.out.println(INSTRUCTIONS);
+		System.out.println();
+		System.out.println(problem.levelOne());
+		if(!score.checkAnswer())
+		{
+			if(score.getTrials() < 3)
+			{
+				System.out.println("Try again. You have "+score.getTrials()+" attempts left.");
+			}
+			else
+			{
+				score.changeLevel(1);
+				System.out.println("Sorry, you lose. Type 'Y' and press enter to start over.");
+			}
+		}
+		else if(score.checkAnswer())
+		{
+			System.out.println("Good job! You got it right.");
+			System.out.println("Your current score is "+score.getScore());
+		}
+		else if(score.checkAnswer()&&score.getScore()==5)
+		{
+			System.out.println("Congrats, you've advanced to the next level!");
+			if(score.getLevel() == 1)
+			{
+				score.changeLevel(2);
+			}
+			else
+				score.changeLevel(3);
+		}
+		else if(score.checkAnswer()&&score.getScore()==5&&score.getLevel()==3)
+		{
+			System.out.println("Congratulations, you've won the game!");
+			System.out.println("Type 'start over' and press 'enter' to start a new game.");
+		}
+		
+		System.out.print("The answer is: "+getAnswer()+".");
 
 	}
 	
@@ -20,10 +61,11 @@ public class UserInteraction
 	 * Returns the user's answer
 	 * @return answer
 	 */
-	public int getAnswer()
+	public static int getAnswer()
 	{
 		return answer;
 	}
+	
 }
 
 
